34972a1be273d046f8a1ace5fa89ccef2d251d9d
Peter Morton <petermorton1247@gmail.com>
2025-03-08 22:37:56 +0000

    Implemented the user registration process in the API and passed all of the related tests

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,28 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    public ResponseEntity<Account> registerAccount(Account account) {
+        if (account.getUsername().isBlank() || account.getPassword().length() < 4) {
+            return ResponseEntity.status(400).body(null);
+        }
+    
+        if (accountRepository.existsByUsername(account.getUsername()) == true) {
+            return ResponseEntity.status(409).body(null);
+        }
+    
+        Account savedAccount = accountRepository.save(account);
+        return ResponseEntity.status(HttpStatus.OK).body(savedAccount);
+    }
 }
